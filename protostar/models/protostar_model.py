e934fa713f5b81efcf28771398f515974646f4c0
Serge Kim <sergevkim@gmail.com>
2021-02-11 23:36:01 +0300

    Initial commit

---
+++ protostar/models/protostar_model.py
@@ -0,0 +1,67 @@
+from typing import List, Tuple
+
+import torch
+from torch import Tensor
+from torch.optim import Adam
+from torch.optim.lr_scheduler import _LRScheduler, StepLR
+from torch.optim.optimizer import Optimizer
+from torchaudio.transforms import MelSpectrogram
+
+from protostar.models import BaseModel
+
+
+class ProtostarModel(BaseModel):
+    def __init__(
+            self,
+            #device: torch.device,
+            #learning_rate: float,
+            #scheduler_step_size: int,
+            #scheduler_gamma: float,
+            #verbose: bool,
+        ):
+        super().__init__()
+        self.ms = MelSpectrogram()
+        from torch.nn import Linear
+        self.l = Linear(12, 12)
+
+    def forward(
+            self,
+            x: Tensor,
+        ) -> Tensor:
+        pass
+
+    def training_step(
+            self,
+            batch: Tensor,
+            batch_idx: int,
+            optimizer_idx: int,
+        ) -> Tensor:
+        pass
+
+    def validation_step(
+            self,
+            batch: Tensor,
+            batch_idx: int,
+        ) -> Tensor:
+        pass
+
+    def configure_optimizers(
+            self,
+        ) -> Tuple[List[Optimizer], List[_LRScheduler]]:
+        optimizer = Adam(
+            params=self.parameters(),
+            lr=self.learning_rate,
+        )
+        scheduler = LambdaLR(
+            optimizer=optimizer,
+            step_size=self.step_size,
+            gamma=self.gamma,
+        )
+
+        return [optimizer], [scheduler]
+
+
+if __name__ == '__main__':
+    m = ProtostarModel()
+    print(m)
+
