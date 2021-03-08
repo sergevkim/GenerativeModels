2c036c7ad58d1449a4c6f3e54bd18a8879a6c22d
Sergei Kim <sergei.kim@wayray.com>
2021-03-09 00:58:10 +0300

    celeba check

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -5,6 +5,12 @@ from geode.datamodules import (
 )
 
 
+from six.moves import urllib
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+urllib.request.install_opener(opener)
+
+
 if __name__ == '__main__':
     omniglot_datamodule = OmniglotDataModule(
         data_path='./data',
@@ -21,7 +27,7 @@ if __name__ == '__main__':
     mnist_datamodule.setup(download=True)
 
     celeba_datamodule = CelebADataModule(
-        data_path='./data',
+        data_path='./data/celeba',
         batch_size=64,
         num_workers=4,
     )
